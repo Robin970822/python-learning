45ea7ca3070649f50f51846bf2f178c94d93a1c4
Robin970822 <717740827@qq.com>
2018-04-19 19:34:01 +0800

    pandas 选择

--- numpy&pandas/pandas_choose.py
+++ numpy&pandas/pandas_choose.py
@@ -5,7 +5,7 @@ s = pd.Series([1, 3, 6, np.nan, 44, 1])
 print s
 
 dates = pd.date_range('20180419', periods=6)
-df = pd.DataFrame(np.random.rand(6, 4),
+df = pd.DataFrame(np.arange(24).reshape((6, 4)),
                   index=dates,
                   columns=['A', 'B', 'C', 'D'])
 
@@ -13,3 +13,14 @@ print df['A']
 print df.A
 print df[0:3]
 print df['20180419':'20180422']
+
+print df.loc['20180419']
+print df.loc[:, ['A', 'B']]
+print df.loc['20180419', ['A', 'B']]
+
+print df.iloc[3, 1]
+print df.iloc[3:5, 1:3]
+print df.iloc[[1, 3, 5], 1:3]
+
+print df.ix[:3, ['A', 'C']]
+print df[df.A > 8]
