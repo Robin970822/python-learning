3858acb1957a6e4c2e938f154e166a8c25069e45
Robin970822 <717740827@qq.com>
2018-12-30 00:31:36 +0800

    绘制色彩三维散点图，添加参数输入

--- matplotlib/plot_color.py
+++ matplotlib/plot_color.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
+import getopt
 import sys
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
-filename = sys.argv[1]
-data = pd.DataFrame(pd.read_excel(filename))
+
+def usage():
+    print "-i: "
+    print "\t name of input file, only excel is supported now;"
+    print "-o: "
+    print "\t name of output file;"
+    print "-h"
+    print "\t get help"
+    return
+
+
+opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
+input_file = ""
+output_file = ""
+for op, value in opts:
+    if op == "-i":
+        input_file = value
+    elif op == "-o":
+        output_file = value
+    elif op == "-h":
+        usage()
+        sys.exit()
+
+if input_file == "":
+    print "Please input a filename!"
+    sys.exit()
+
+data = pd.DataFrame(pd.read_excel(input_file))
 print data
 
 fig = plt.figure()
@@ -26,5 +53,9 @@ ax.set_xlabel(columns[0])
 ax.set_ylabel(columns[1])
 ax.set_zlabel(columns[2])
 
-plt.show()
+if output_file == "":
+    plt.savefig('fig.png', bbox_inches='tight')
+else:
+    plt.savefig(output_file, bbox_inches='tight')
 
+plt.show()
